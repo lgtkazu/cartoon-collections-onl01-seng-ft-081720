d2034d5a493b67ac72b910d72654661a17d39afa
Seth Mariscotes <seth.mariscotes@yahoo.com>
2020-08-26 15:56:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
 def roll_call_dwarves# code an argument here
-  
+  def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length
+    puts "#{i + 1}. #{dwarves[i]}"
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
